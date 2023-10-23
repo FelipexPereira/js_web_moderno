d10ec74b62e968d882abbea5f53ae599b874965d
FelipexPereira <felipegabriel722@gmail.com>
2023-10-23 11:53:27 -0300

    mm

--- exercicios-js/fundamentos/aritmeticos.js
+++ exercicios-js/fundamentos/aritmeticos.js
@@ -1,3 +1,9 @@
 const [a, b, c, d] = [3, 5, 1, 15];
-const soma = a + b + c + d;
 
+const soma = a + b + c + d;
+const subtracao = b - d;
+const multiplicacao = a * b;
+const divicao = d / a;
+const modulo = a % 2;
+
+console.log(soma, subtracao, divicao, multiplicacao, modulo)
