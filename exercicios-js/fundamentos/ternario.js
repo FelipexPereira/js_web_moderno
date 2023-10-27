320efbe05b814b1b71e340e7bdcd7175490382f8
FelipexPereira <felipegabriel722@gmail.com>
2023-10-26 22:39:40 -0300

    controle

---
+++ exercicios-js/fundamentos/ternario.js
@@ -0,0 +1,8 @@
+// condição ? expressão_se_verdadeira : expressão_se_falsa
+var idade = 18;
+var mensagem = idade >= 18 ? "Você é maior de idade" : "Você é menor de idade";
+console.log(mensagem);
+
+const resultado = (nota) => (nota >= 7 ? "Aprovado" : "Reprovado");
+console.log(resultado(7.1));
+console.log(resultado(6.9));
